ee48b1596fb14e5b556a58ddd3bc58c8caa9efc2
Steemuh <mackobuenviaje824@gmail.com>
2026-04-22 15:11:58 +0800

    Customer CSS retrieve Apr 22, 3:22pm

--- apps/storefront/src/app/layout.tsx
+++ apps/storefront/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import { type ReactNode } from "react";
 import { type Metadata } from "next";
-
+import './globals.css';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
